c9674dee3a71576239e71289ff7e862a274ae7a0
Thomas_Hansen <ctrhansen@yahoo.com>
2019-05-06 16:08:40 -0500

    new stuff

--- cms-app/config/routes.rb
+++ cms-app/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :athletes
+  resources :competitions
+  resources :events
+  resources :scores
+  resources :teams
+  
 end
